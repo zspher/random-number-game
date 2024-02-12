5fe241a7d6711268b10b2ff911853a247e94ef70
zspher <66728045+zspher@users.noreply.github.com>
2024-02-12 20:47:54 +0800

    feat: init avalonia

---
+++ src/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace random_number_game.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ src/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace random_number_game.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
